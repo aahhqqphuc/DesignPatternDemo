c950683866b1aeb3d3893eab97072ba57bb5584d
Hoàng Phúc <ffqphuc@gmail.com>
2021-11-17 16:40:48 +0700

    Add project files.

---
+++ Builder/Program.cs
@@ -0,0 +1,122 @@
+﻿namespace Builder
+{
+    // Chỉ định phương thức để tạo các part khác nhau của đối tượng product
+    public interface IBuilder
+    {
+        void BuildPartA();
+
+        void BuildPartB();
+
+        void BuildPartC();
+    }
+
+    public class ConcreteBuilder : IBuilder
+    {
+        private Product _product = new Product();
+
+        public ConcreteBuilder()
+        {
+            this.Reset();
+        }
+
+        public void Reset()
+        {
+            this._product = new Product();
+        }
+
+        public void BuildPartA()
+        {
+            this._product.Add("PartA1");
+        }
+
+        public void BuildPartB()
+        {
+            this._product.Add("PartB1");
+        }
+
+        public void BuildPartC()
+        {
+            this._product.Add("PartC1");
+        }
+
+        public Product GetProduct()
+        {
+            Product result = this._product;
+
+            this.Reset();
+
+            return result;
+        }
+    }
+
+    public class Product
+    {
+        private List<object> _parts = new List<object>();
+
+        public void Add(string part)
+        {
+            this._parts.Add(part);
+        }
+
+        public string ListParts()
+        {
+            string str = string.Empty;
+
+            for (int i = 0; i < this._parts.Count; i++)
+            {
+                str += this._parts[i] + ", ";
+            }
+
+            str = str.Remove(str.Length - 2); // xóa ", " ở cuối
+
+            return "Product parts: " + str + "\n";
+        }
+    }
+
+    // optional
+    public class Director
+    {
+        private IBuilder _builder;
+
+        public IBuilder Builder
+        {
+            set { _builder = value; }
+        }
+
+        public void BuildMinimalViableProduct()
+        {
+            this._builder.BuildPartA();
+        }
+
+        public void BuildFullFeaturedProduct()
+        {
+            this._builder.BuildPartA();
+            this._builder.BuildPartB();
+            this._builder.BuildPartC();
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var director = new Director();
+            var builder = new ConcreteBuilder();
+            director.Builder = builder;
+
+            Console.WriteLine("Standard basic product:");
+            director.BuildMinimalViableProduct();
+            Console.WriteLine(builder.GetProduct().ListParts());
+
+            Console.WriteLine("Standard full featured product:");
+            director.BuildFullFeaturedProduct();
+            Console.WriteLine(builder.GetProduct().ListParts());
+
+            // Builder pattern có thể sử dụng mà không cần class Director
+            Console.WriteLine("Custom product:");
+            builder.BuildPartA();
+            builder.BuildPartC();
+            Console.Write(builder.GetProduct().ListParts());
+        }
+    }
+}
